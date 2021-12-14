d4cf21539b001f0bbe0b85690c2bafec65870686
Arnoud de Boer <a.deboer@geonovum.nl>
2021-12-14 15:01:27 +0100

    Update colofon.js

--- werkafspraken/bgt-berichtenverkeer/media/colofon.js
+++ werkafspraken/bgt-berichtenverkeer/media/colofon.js
@@ -1,37 +1,20 @@
 var respecConfig = {
-  specStatus: "GN-VV",
+  specStatus: "GN-WV",
   specType: "WA",
   //canonicalURI: "WV",
-  publishDate: "2018-07-01",
+  //publishDate: "2018-07-01",
   editors: [    
     {
       company: "Geonovum",
       companyURL: "http://www.geonovum.nl/",
       mailto: "imgeo@geonovum.nl",    
-    },
-    {
-      company: "Ministerie BZK",
-      companyURL: "https://www.geobasisregistraties.nl/"
-    },
-    {
-      company: "VNG Realisatie",
-      companyURL: "https://www.vngrealisatie.nl/",          
-    },
-    {
-      company: "SVB-BGT",
-      companyURL: "http://www.svb-bgt.nl/"   
-    },    
-    {
-      company: "Kadaster",
-      companyURL: "https://www.kadaster.nl/bag",
-    }
-    
+    }    
   ],
   shortName: "IMGeo",
   pubDomain: "IMGeo",
   //previousURI: "https://www.geonovum.nl/BRO",
   github: "https://github.com/Geonovum/IMGeo",
-  previousPublishDate: "2017-01-01",
+ // previousPublishDate: "2017-01-01",
   //previousStatus: "GN-WV",
   //localBiblio: {
   //  "ISO19136": {
