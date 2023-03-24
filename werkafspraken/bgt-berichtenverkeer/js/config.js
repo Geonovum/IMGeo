e05ce26bf5047122a3053f78e80cfcc182cf581f
Arnoud de Boer <a.deboer@geonovum.nl>
2023-03-24 09:37:11 +0100

    update naar nieuwe Respec

---
+++ werkafspraken/bgt-berichtenverkeer/js/config.js
@@ -0,0 +1,43 @@
+let respecConfig = {
+  useLogo: true,
+  useLabel: true,
+  license: "cc0",
+  specStatus: "WV",
+  specType: "HR",
+  pubDomain: "dk",
+  shortName: "template",
+  publishDate: "2022-06-24",
+  publishVersion: "0.0.2",
+  
+  previousVersion: "0.0.1",
+  previousPublishDate: "2014-05-01",
+  previousMaturity: "EO",
+  title: "titleText",
+  content: {"ch01": "informative", "ch02": "", "mermaid": ""},
+  editors:
+    [
+      {
+        name: "Logius Standaarden",
+        company: "Logius",
+        companyURL: "https://github.com/Logius-standaarden",
+      }
+    ],
+  authors:
+    [
+      {
+        name: "Logius Standaarden",
+        company: "Logius",
+        companyURL: "https://github.com/Logius-standaarden",
+      }
+    ],
+  github: "https://github.com/Logius-standaarden/ReSpec-template",
+
+
+  // Create PDF and link to file in header (optional):
+  alternateFormats: [
+      {
+          label: "pdf",
+          uri: "template.pdf",
+      },
+  ],
+};
