56b16da7ac5203d6eb1ce2b1266ea956be63d440
Arnoud de Boer <a.deboer@geonovum.nl>
2023-09-18 09:18:34 +0200

    Update config.js

--- werkafspraken/opdelen-panden/deels-toestaan/js/config.js
+++ werkafspraken/opdelen-panden/deels-toestaan/js/config.js
@@ -53,17 +53,12 @@ let respecConfig = {
   //-- de namen van de auteur(s) ------------------------------------------------------
   //-- vul in: per auteur: name:, company:, companyURL: -------------------------------
   authors:
-    [
-    {  name: "Michel van de Spijker",
-	   company: "Gemeente Best",
-	   companyURL: "https://www.gemeentebest.nl/"
-	},
+    [    
 	{
         name: "Silvy Horbach",
         company: "Geonovum",
         companyURL: "https://www.geonovum.nl",
-      },
-	  
+      },	  
 	  {
         name: "Arnoud de Boer",
         company: "Geonovum",
